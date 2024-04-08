3952154881dd4dab8ad3dba0c147639c49fb090a
tuutoo <tuutoo@gmail.com>
2024-04-08 22:57:49 +0800

    feat: 🎸 added pridict path for ai blackboard detection

--- api/main.py
+++ api/main.py
@@ -1,11 +1,15 @@
 from mangum import Mangum
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from PIL import Image
 import numpy as np
 import io
+
+from ultralytics import YOLO
+import uuid
+import math
 
 app = FastAPI()
 
@@ -17,6 +21,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 假设你的YOLOv9模型加载方式
+# 请根据实际情况替换成适合你的模型的代码
+model = YOLO('blackboard_best.pt')
+
+# Display model information (optional)
+model.info()
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World"}
@@ -24,6 +35,52 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    # 将上传的文件读取为字节
+    image_bytes = await file.read()
+
+    # 使用PIL读取图片
+    image = Image.open(io.BytesIO(image_bytes))
+
+    # # 确保图片是RGB格式
+    # if image.mode != 'RGB':
+    #     image = image.convert('RGB')
+
+    # 使用模型进行预测
+    results = model.predict(image)
+
+    # 打印results的结构来查看
+    print(results[0].boxes)
+
+    # 将结果处理为可以序列化的格式
+    predictions = format_predictions(results)
+
+    return JSONResponse(content={"predictions": predictions})
+
+    # 根据results的实际结构进行处理...
+def format_predictions(results):
+    predictions = []
+    # 假设results是一个列表，其中每个元素代表一张图片的预测结果
+    for result in results:
+        # 访问每个结果的boxes属性
+        boxes = result.boxes
+        # 检查是否有检测到的对象
+        if boxes.data is not None and len(boxes.data) > 0:
+            for i in range(len(boxes.data)):
+                x1, y1, x2, y2, conf, cls = boxes.data[i]
+                predictions.append({
+                    "x": round(x1.item(), 1),  # 保留一位小数
+                    "y": round(y1.item(), 1),  # 保留一位小数
+                    "width": math.ceil((x2 - x1).item()),  # 向上取整
+                    "height": math.ceil((y2 - y1).item()),  # 向上取整
+                    "confidence": round(conf.item(), 3),  # 保留三位小数
+                    "class": result.names[int(cls.item())],  # 使用索引从names中获取类名
+                    "class_id": int(cls.item()),
+                    "detection_id": str(uuid.uuid4())
+                })
+    return predictions
 
 @app.post("/invert/")
 async def invert_image(file: UploadFile = File(...)):
